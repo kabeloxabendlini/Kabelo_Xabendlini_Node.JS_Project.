5d92af1d3ffcb1421f71b54622493fa11b8a8fe1
kabelo385 <kabeloxabendlini385@gmail.com>
2026-03-26 17:13:38 +0200

    fix: remove .env.example from tracking

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,43 +1,8 @@
-const bcrypt = require('bcrypt');
-const User = require('../models/User');
-
-module.exports = async (req, res) => {
-    const { username, password } = req.body;
-
-    try {
-        // Find the user by username
-        const user = await User.findOne({ username });
-        if (!user) {
-            req.flash('loginError', 'Invalid username or password');
-            req.flash('data', { username }); // keep username in form
-            return res.redirect('/auth/login');
-        }
-
-        // Compare password
-        const passwordMatch = await bcrypt.compare(password, user.password);
-        if (!passwordMatch) {
-            req.flash('loginError', 'Invalid username or password');
-            req.flash('data', { username });
-            return res.redirect('/auth/login');
-        }
-
-        // Login successful
-        req.session.userId = user._id;
-
-        req.session.save(err => {
-            if (err) {
-                console.error(err);
-                req.flash('loginError', 'Something went wrong. Please try again.');
-                return res.redirect('/auth/login');
-            }
-
-            // redirect to home page after successful login
-            return res.redirect('/');
-        });
-
-    } catch (error) {
-        console.error(error);
-        req.flash('loginError', 'Something went wrong. Please try again.');
+// middleware/authMiddleware.js
+module.exports = (req, res, next) => {
+    // If user is NOT logged in, redirect to login page
+    if (!req.session || !req.session.userId) {
         return res.redirect('/auth/login');
     }
+    next(); // user is logged in, allow access
 };
